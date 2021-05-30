7598857a873e67acadd4e7fe4e97817159839c38
mayank190821 <mayank.123.2000@gmail.com>
2021-05-30 20:09:44 +0530

    added cpp file which we have to submit

---
+++ arrayAndPeaks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=1;i<=n;i++)
+
+
+int main(){
+    int t;
+    cin>>t;
+    rep(i,t){
+        int n,k;
+        if(n==1){
+            cout<<n;
+        }
+        
+    }
+    return 0;
+}
